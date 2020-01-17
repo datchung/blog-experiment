949e8f97319f815c4075bd74e46217f9ffa985a3
datchung <datxchung@gmail.com>
2020-01-16 23:32:52 -0500

    Move Data directory

--- src/Api/PostApi.js
+++ src/Api/PostApi.js
@@ -1,5 +1,5 @@
 import { handleResponse, handleError } from "./ApiUtils";
-const baseUrl = process.env.API_URL + "/src/Data/posts.json";
+const baseUrl = process.env.API_URL + "/data/posts.json";
 
 export function getPosts() {
   return fetch(baseUrl)
