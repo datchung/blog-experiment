094fb68d97139738dac7b496d7e8d9428ac6e4fc
datchung <datxchung@gmail.com>
2020-01-16 23:34:08 -0500

    Simplify base url

--- src/Api/PostApi.js
+++ src/Api/PostApi.js
@@ -1,5 +1,6 @@
 import { handleResponse, handleError } from "./ApiUtils";
-const baseUrl = process.env.API_URL + "/data/posts.json";
+// responseconst baseUrl = process.env.API_URL + "/data/posts.json";
+const baseUrl = "/data/posts.json";
 
 export function getPosts() {
   return fetch(baseUrl)
