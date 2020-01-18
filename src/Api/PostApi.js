d263b272d6aa9e7b97343a55a4269ec03eaa6744
datchung <datxchung@gmail.com>
2020-01-18 15:41:01 -0500

    Ditto

--- src/Api/PostApi.js
+++ src/Api/PostApi.js
@@ -1,5 +1,5 @@
 import { handleResponse, handleError } from "./ApiUtils";
-const baseUrl = process.env.API_URL + "/data/posts.json";
+const baseUrl = process.env.API_URL + "data/posts.json";
 //const baseUrl = "/data/posts.json";
 
 export function getPosts() {
