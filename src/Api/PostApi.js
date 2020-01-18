67a905dd9ed877441ab0ebbb2a8191cf0764e964
datchung <datxchung@gmail.com>
2020-01-18 15:24:17 -0500

    Add blog API prefix

--- src/Api/PostApi.js
+++ src/Api/PostApi.js
@@ -1,6 +1,6 @@
 import { handleResponse, handleError } from "./ApiUtils";
-// responseconst baseUrl = process.env.API_URL + "/data/posts.json";
-const baseUrl = "/data/posts.json";
+const baseUrl = process.env.API_URL + "/data/posts.json";
+//const baseUrl = "/data/posts.json";
 
 export function getPosts() {
   return fetch(baseUrl)
